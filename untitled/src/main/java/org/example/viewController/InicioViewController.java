88eb0c40cc7565119ab47413a92a72090d852212
laux-her <262458733+laxra-h@users.noreply.github.com>
2026-09-10 01:57:42 -0500

    Cree vista inicial fx

--- untitled/src/main/java/org/example/viewController/InicioViewController.java
+++ untitled/src/main/java/org/example/viewController/InicioViewController.java
@@ -1,4 +1,22 @@
 package org.example.viewController;
 
 public class InicioViewController {
+
+    public void abrirClientes() {
+    }
+
+    public void abrirBicicletas() {
+    }
+
+    public void abrirMecanicos() {
+    }
+
+    public void abrirNuevaOrden() {
+    }
+
+    public void abrirHistorial() {
+    }
+
+    public void abrirOrdenesDia() {
+    }
 }
