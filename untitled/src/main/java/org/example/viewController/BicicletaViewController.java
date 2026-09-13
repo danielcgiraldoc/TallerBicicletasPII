bde0a9336b9c08b3e966f6a8dd1ec9223cd7e71a
laux-her <262458733+laxra-h@users.noreply.github.com>
2026-09-12 22:19:27 -0500

    Cree vista de mecanico y bicicleta

---
+++ untitled/src/main/java/org/example/viewController/BicicletaViewController.java
@@ -0,0 +1,31 @@
+package org.example.viewController;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class BicicletaViewController {
+
+    @FXML
+    private TextField txtMarca;
+
+    @FXML
+    private ComboBox<String> cmbTipoBicicleta;
+
+    @FXML
+    private TextField txtColor;
+
+    @FXML
+    private TextField txtSerial;
+
+    @FXML
+    private TextField txtAntiguedad;
+
+    @FXML
+    private void guardarBicicleta() {
+    }
+
+    @FXML
+    private void volverInicio() {
+    }
+}
---
+++ untitled/src/main/java/org/example/viewController/MecanicoViewController.java
@@ -0,0 +1,25 @@
+package org.example.viewController;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class MecanicoViewController {
+
+    @FXML
+    private TextField txtNombreMecanico;
+
+    @FXML
+    private ComboBox<String> cmbEspecialidad;
+
+    @FXML
+    private TextField txtCodigo;
+
+    @FXML
+    private void guardarMecanico() {
+    }
+
+    @FXML
+    private void volverInicio() {
+    }
+}
